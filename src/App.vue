ddd28664c2f1b0a387fe2052875b36a8fcb74e17
ThibaultYVD <thibaultyvard01@gmail.com>
2024-12-20 19:10:29 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,83 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <AppNavbar />
+    <div class="main-container">
+      <div class="top">
+        Hi and welcome on my website! You can find here my projects and the services hosted on this
+        server. If you want to learn more about me and my projects, please take a look at my Linkedin and my Github
+        accounts.
+      </div>
+      <div class="center-container">
+        <div class="projects-container">
+          <h1>Projects</h1>
+          <p>
+            Thoses projects are developed by me and run on this server. You can try them if you want!
+          </p>
+          <QuicklinksList :items="projects" />
+        </div>
+        <div class="quicklinks-container">
+          <h1>Services</h1>
+          <p>Those services are hosted on my server. Only verified people can use them.</p>
+          <QuicklinksList :items="quicklinks" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppNavbar from "./components/AppNavbar.vue";
+import QuicklinksList from "./components/List.vue";
+
+// Importation du JSON local
+import data from "@/assets/data.json";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    AppNavbar,
+    QuicklinksList,
+  },
+  data() {
+    return {
+      projects: data.projects,
+      quicklinks: data.quicklinks, // Récupère la partie quicklinks
+
+    };
+  },
+};
 </script>
 
 <style>
+body {
+  margin: 0px;
+  padding: 0px;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+}
+
+.main-container {
+  display: flex;
+  flex-direction: column;
+  padding-top: 50px;
+  padding-left: 200px;
+  padding-right: 200px;
+  gap: 50px;
+}
+
+.center-container {
+  display: flex;
+  gap: 30px;
+}
+
+.projects-container,
+.quicklinks-container {
+  width: 50%;
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.75);
+  padding: 20px;
 }
 </style>
---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,31 @@
+<template>
+    <nav class="navbar">
+        <div class="navbar-brand">
+            <h1>Thibault's server homepage</h1>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: "AppNavbar", // Nom multi-word du composant
+};
+</script>
+
+<style scoped>
+.navbar {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #2c3e50;
+    color: white;
+    padding: 1rem;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.navbar-brand h1 {
+    margin: 0;
+    font-size: 1.5rem;
+    font-weight: bold;
+}
+</style>
